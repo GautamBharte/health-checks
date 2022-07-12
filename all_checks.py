7c31e010dd5877a5f5aab0ecfb47532a56320d06
Gautam Kailas Bharte <gautambharte123@gmail.com>
2022-07-12 07:31:00 +0530

    Add function to check CPU usage

--- all_checks.py
+++ all_checks.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import sys
 import socket
+import psutil
 
 def check_reboot():
     """Returns True if the computer has a pending reboot."""
@@ -24,6 +25,11 @@ def check_root_full():
     return check_dis_full(disk="/", min_gb=2, min_percent=10)
 
 
+def check_cpu_constrained():
+    """Returns True if the cpu is having too much usage, False otherwise"""
+    return psutil.cpu_percent(1) > 75
+
+
 def check_no_network():
     """Return True ud ut fails to resolve Google's URL, False otherwise"""
     try:
@@ -37,6 +43,7 @@ def main():
     checks = [
         (check_reboot, "Pending Reboot"),
         (check_root_full, "Root partition full"),
+        (check_cpu_constrained, "CPU load too high."),
         (check_no_network, "No working network."),
     ]
     everything_ok = True
